eacd552b553120630231ecfda954a29f7fff0dc5
Niels Mokkentstorm <nielsmokkenstorm@gmail.com>
2019-12-07 19:55:17 +0100

    Add initial project scaffolding

---
+++ main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const {app, BrowserWindow} = require('electron');
+
+require('electron-reload')(__dirname);
+
+function createWindow() {
+  // Create the browser window.
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  // and load the index.html of the app.
+  win.loadFile('index.html');
+}
+
+app.on('ready', createWindow);
